1252c2182d24c52161fa5c2429f60eee88701d26
Jasper De Luna <jasper.deluna@kyckr.com>
2021-09-02 23:50:25 +0100

    Data layer

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -1,7 +1,44 @@
-﻿namespace PlatformService.Data
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Model;
+
+namespace PlatformService.Data
 {
      public static class PrepDb
     {
-        
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding data");
+                context.Platforms.AddRange(
+                    new Platform()
+                    {
+                        Name = "Dot Net", Publisher = "Microsoft", Cost = "free",
+                    },  new Platform()
+                    {
+                        Name = "Docker", Publisher = "Microsoft", Cost = "free",
+                    }, new Platform()
+                    {
+                        Name = "Kubernetes", Publisher = "Oracle", Cost = "free",
+                    }
+                );
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("We have data");
+            }
+        }
     }
 }
